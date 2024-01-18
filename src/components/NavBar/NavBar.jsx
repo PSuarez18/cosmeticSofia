8cf2a64fe3ecc7acd554b0a10a04ace3fe06db14
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2024-01-18 14:57:19 -0300

    navbarLista responsive total

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../../styles/NavBar.css'
+import MenuDrop from '../MenuNavBarDrop/MenuNavBarDrop';
 
 const NavBar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+  
+  console.log(menuOpen)
   return (
     <nav className='navbar'>
       <div className='container-logoNails'>
         <a href="/"><img src="src\assets\images\nailsLogo.png" alt="" /></a>
       </div>
-      <ul className='navbar-list'>
+
+      <MenuDrop menuOpen={menuOpen} />
+      <ul className="navbar-list">
         <li className='navbar-item'>
           <Link to="/" className='navbar-link'>Inicio</Link>
         </li>
+        
         <li className='navbar-item'>
           <Link to="/servicios" className='navbar-link'><span>Servicios</span></Link>
         </li>
@@ -27,6 +39,9 @@ const NavBar = () => {
       </ul>
       <div className='container-logoMarket'>
         <a href="/"><img className="logoMarket" src="src\assets\images\logoMarket.png" alt="" /></a>
+      </div>
+      <div className='menu-icon' onClick={toggleMenu}>
+        <img src="src\assets\images\menu.png" alt="menuToggle" />
       </div>
     </nav>
   )
