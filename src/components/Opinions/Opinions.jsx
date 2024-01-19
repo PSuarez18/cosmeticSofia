b9fe928a4da7c29b6f59301f432f091f2a418bff
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2024-01-18 23:31:58 -0300

    cambio final para el deploy

--- src/components/Opinions/Opinions.jsx
+++ src/components/Opinions/Opinions.jsx
@@ -5,70 +5,70 @@ import user from '../../assets/images/user.jpg';
 import { CSSTransition } from 'react-transition-group';
 
 const Opinions = () => {
-    const opinionsData = [
-        {
-          id: 1,
-          content: "First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable.",
-          author: "Dardo fuseneco",
-        },
+  const opinionsData = [
+    {
+      id: 1,
+      content: "¡Absolutamente impresionante! La artesanía en las uñas está más allá de la imaginación. Me sentí como una reina con mi nuevo juego de uñas. La atención al detalle es inigualable. ¡No puedo esperar para presumirlas!",
+      author: "Isabella Smith",
+    },
 
-        {
-            id: 2,
-            content: "First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable.",
-            author: "Jorge Damian",
-          },
+    {
+      id: 2,
+      content: "¡Transformación de uñas asombrosa! Las elecciones de color y los diseños son fuera de este mundo. Es como una obra de arte en mis dedos. Los cumplidos no han dejado de llegar. ¡Altamente recomendado!",
+      author: "Sophie Anderson",
+    },
 
-          {
-            id: 3,
-            content: "First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable.",
-            author: "Alan Otero",
-          },
+    {
+      id: 3,
+      content: "¡Increíble experiencia con las uñas! La creatividad y habilidad mostradas al hacer mis uñas fueron excepcionales. Salí sintiéndome segura y fabulosa. La atmósfera en el salón es tan acogedora. ¡Definitivamente volveré!",
+      author: "Michael Rodriguez",
+    },
 
-          {
-            id: 4,
-            content: "First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable. First time trying it out, I loved the cleanliness. Great customer service and affordable.",
-            author: "Jose Dominguez",
-          },
-        
-      ];
+    {
+      id: 4,
+      content: "¡Increíble magia con las uñas! No sabía que mis uñas podían lucir tan bien. La precisión del técnico es inigualable. Todo el proceso fue relajante y los resultados hablan por sí mismos. ¡10/10!",
+      author: "Emily Davis",
+    },
 
-      const [currentSlide, setCurrentSlide] = useState(0);
-      const [activateTransition, setActivateTransition] = useState(true);
+  ];
 
-      useEffect(() => {
-        const intervalId = setInterval(() => {
-          setActivateTransition(false);
-    
-          setTimeout(() => {
-            nextSlide();
-            setActivateTransition(true);
-          }, 1000); // Ajusta el tiempo según sea necesario
-        }, 4000);
-    
-        return () => {
-          clearInterval(intervalId);
-        };
-      }, [currentSlide]);
-    
-      const nextSlide = () => {
-        setCurrentSlide((prevSlide) => (prevSlide + 1) % opinionsData.length);
-      };
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [activateTransition, setActivateTransition] = useState(true);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setActivateTransition(false);
+
+      setTimeout(() => {
+        nextSlide();
+        setActivateTransition(true);
+      }, 1000); // Ajusta el tiempo según sea necesario
+    }, 9000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentSlide]);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % opinionsData.length);
+  };
 
 
   return (
-    <section className='sectionOpinion'> 
-    <div className='contenedor-opiniones'>
-      <div className='contenedorFirst'>
-        <div className='contenedorTitulo'>  
-          <h2 className='tituloOpinion'>
-            Escucha Lo Que Nuestras Clientes Tienen Que Decir
-          </h2>
-          <div className='botonOpinion'>
-            <button className='primary-button'>Contactanos</button>
-          </div> 
-        </div>   
-      </div>
-      <div className='contenedorSecond'>
+    <section className='sectionOpinion'>
+      <div className='contenedor-opiniones'>
+        <div className='contenedorFirst'>
+          <div className='contenedorTitulo'>
+            <h2 className='tituloOpinion'>
+              Escucha Lo Que Nuestras Clientes Tienen Que Decir
+            </h2>
+            <div className='botonOpinion'>
+              <button className='primary-button'>Contactanos</button>
+            </div>
+          </div>
+        </div>
+        <div className='contenedorSecond'>
           <div className='contenedorSlide'>
             <CSSTransition
               in={activateTransition}
